95361d28f6a24f6902f706a1a2b035f15c712404
Briman-Jag <brianstreetman25@gmail.com>
2019-11-08 15:52:42 -0700

    Added packages

--- JPABrews/src/main/java/com/skilldistillery/brews/entities/Brew.java
+++ JPABrews/src/main/java/com/skilldistillery/brews/entities/Brew.java
@@ -21,6 +21,54 @@ public class Brew {
 	
 	private double abv;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBreweryName() {
+		return breweryName;
+	}
+	public void setBreweryName(String breweryName) {
+		this.breweryName = breweryName;
+	}
+	public String getBreweryLocation() {
+		return breweryLocation;
+	}
+	public void setBreweryLocation(String breweryLocation) {
+		this.breweryLocation = breweryLocation;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public double getAbv() {
+		return abv;
+	}
+	public void setAbv(double abv) {
+		this.abv = abv;
+	}
+	public int getIbu() {
+		return ibu;
+	}
+	public void setIbu(int ibu) {
+		this.ibu = ibu;
+	}
+	public double getMyRating() {
+		return myRating;
+	}
+	public void setMyRating(double myRating) {
+		this.myRating = myRating;
+	}
+	public Date getDateRated() {
+		return dateRated;
+	}
+	public void setDateRated(Date dateRated) {
+		this.dateRated = dateRated;
+	}
 	private int ibu;
 	private double myRating;
 	private Date dateRated;
