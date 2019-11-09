fdc8237849979757fbd7972dd8ff04d22f621861
Briman-Jag <brianstreetman25@gmail.com>
2019-11-09 11:26:45 -0700

    Fixed build.gradle in JPABrews. Also fixed breweryname and alcoholbyvolume names in MySQL table.

---
+++ BriansBrews/src/main/resources/com/skilldistillery/brews/controllers/BrewController.java
@@ -0,0 +1,42 @@
+package com.skilldistillery.brews.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BrewController {
+	
+	@Autowired
+	private BrewDAO brewDAO;
+	
+	@RequestMapping(path="/", method = RequestMethod.GET)
+	public String index(Model model) {
+	  List<Brew> allBrews = brewDAO.findAll();
+	  model.addAttribute("allFilms", allBrews);
+	  
+	  return "index";
+	}
+
+	@RequestMapping(path = "getFilm.do", method = RequestMethod.GET)
+	public ModelAndView getFilm(@RequestParam("fid") int bid) {
+		ModelAndView mv = new ModelAndView();
+
+		Brew brew = brewDAO.findById(bid);
+		// film is unmanaged after it is outside of the transaction that exists in the
+		// DAO
+
+		mv.addObject("brew", brew);
+		mv.setViewName("brew");
+		return mv;
+	}
+
+
+
+}
--- DB/brewsdb.sql
+++ DB/brewsdb.sql
@@ -16,13 +16,19 @@ CREATE SCHEMA IF NOT EXISTS `brewsdb` DEFAULT CHARACTER SET utf8 ;
 USE `brewsdb` ;
 
 -- -----------------------------------------------------
--- Table `Brews`
+-- Table `Brew`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Brews` ;
+DROP TABLE IF EXISTS `Brew` ;
 
-CREATE TABLE IF NOT EXISTS `Brews` (
+CREATE TABLE IF NOT EXISTS `Brew` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `beer_nam` VARCHAR(45) NOT NULL,
+  `beer_name` VARCHAR(45) NOT NULL,
+  `brewery_name` VARCHAR(45) NOT NULL,
+  `brewery_location` VARCHAR(45) NULL,
+  `style` VARCHAR(45) NOT NULL,
+  `alcohol_by_volume` INT NULL,
+  `international_bitterness_units` INT NULL,
+  `my_rating` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -38,11 +44,20 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `Brews`
+-- Data for table `Brew`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `brewsdb`;
-INSERT INTO `Brews` (`id`, `beer_nam`) VALUES (1, 'Habenero Sculpin');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (1, 'Habenero Sculpin', 'Stone Brewing', 'San Diego, CA', 'IPA', 7, 70, '5.0');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (2, 'Liquid Bliss', 'Terrapin Beer Co.', 'Athens, GA', 'Porter', 6.1, 30, '5.0');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (3, 'Peanut Butter Milk Stout', 'Belching Beaver Brewery', 'San Diego, CA', 'Stout', 5.3, 30, '5.0');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (4, 'Dunkelweizen', 'Culture Brewing Company', 'Solana Beach, CA', 'Dunkelweizen', 5.2, 16, '4.5');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (5, 'Tangering Dreamsicle', 'Terrapin Beer Co.', 'Athen, GA', 'IPA', 6.5, 80, '4.5');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (6, '#9', 'Magic Hat Brewing Company', 'South Burlington, VT', 'Pale Ale', 5.1, 20, '4.5');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (7, 'Pinner', 'Oskar Blues Brewery', 'Longmont, CO', 'Session IPA', 4.9, 35, '4.0');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (8, 'Warrior IPA', 'Left Hand Brewing Company', 'Longmont, CO', 'IPA', 7.3, 60, '5.0');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (9, 'Maplesaurus Rex', 'Pure Project Brewing', 'San Diego, CA', 'Imperial Stout', 14, NULL, '4.75');
+INSERT INTO `Brew` (`id`, `beer_name`, `brewery_name`, `brewery_location`, `style`, `alcohol_by_volume`, `international_bitterness_units`, `my_rating`) VALUES (10, 'Barrel-Aged \"On Fire, Jaguar\"', 'Automatic Brewing Co.', 'San Diego, CA', 'Sour', 6.3, NULL, '5.0');
 
 COMMIT;
 
